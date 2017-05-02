360c77d1bb6b48846b7753588e34f94316beefcd
Alexander Ivanov <alxschwrz@gmail.com>
2017-05-02 15:47:47 +0300

    delete comments

--- fd-open.go
+++ fd-open.go
@@ -15,15 +15,12 @@ import (
 	"collectd.org/exec"
 )
 
-// check for errors and bail out if one is hit
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-// find all directories in /proc with a numeric name corresponding to a process id
-// return a slice consisting of directory names for all found numeric process ids
 func listProcs(u *string) []string {
 	mainDir := "/proc"
 	procFiles, err := ioutil.ReadDir(mainDir)
@@ -37,7 +34,7 @@ func listProcs(u *string) []string {
 	for _, fileInfo := range procFiles {
 		isDir := fileInfo.IsDir()
 		fileName := fileInfo.Name()
-		stats, _ := os.Stat(mainDir + "/" + fileName)
+		stats, err := os.Stat(mainDir + "/" + fileName)
 		check(err)
 		fileUid := stats.Sys().(*syscall.Stat_t).Uid
 		matched, _ := regexp.MatchString("^[0-9]+$", fileName)
@@ -48,16 +45,12 @@ func listProcs(u *string) []string {
 	return dirs
 }
 
-// count the number of files in /proc/n/fd where n is a process id like 12345
-// return the length as an int of the ioutil.ReadDir result
 func countFds(dir string) int {
 	fds, err := ioutil.ReadDir("/proc/" + dir + "/fd")
 	check(err)
 	return len(fds)
 }
 
-// take a slice of dirs, iterate over and find number of open files in each
-// add the result and return the total number of open files found in /proc/**/fd
 func countOpenFiles(dirs []string) int {
 	openFiles := 0
 	for _, dir := range dirs {
@@ -73,9 +66,6 @@ func countPercent(opened int) float64 {
 	return float64(opened) * 100 / float64(rLimit.Max)
 }
 
-// get a list of dirs in /proc corresponding to process ids
-// recurse through each and calculate number of open files
-// print the result
 func main() {
 	var username = flag.String("u", "user", "open files of username")
 	flag.Parse()

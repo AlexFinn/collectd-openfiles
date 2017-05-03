b494a47228f229a767f7e63d7d6ac12ea49458ce
Alexander Ivanov <alxschwrz@gmail.com>
2017-05-03 17:01:23 +0300

    fixed

--- fd-open.go
+++ fd-open.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"context"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/user"
 	"regexp"
 	"strconv"
+	"strings"
 	"syscall"
-	"time"
-
-	"collectd.org/api"
-	"collectd.org/exec"
 )
 
 func check(e error) {
@@ -35,7 +32,9 @@ func listProcs(u *string) []string {
 		isDir := fileInfo.IsDir()
 		fileName := fileInfo.Name()
 		stats, err := os.Stat(mainDir + "/" + fileName)
-		check(err)
+		if err != nil {
+			continue
+		}
 		fileUid := stats.Sys().(*syscall.Stat_t).Uid
 		matched, _ := regexp.MatchString("^[0-9]+$", fileName)
 		if isDir == true && matched == true && userid == strconv.Itoa(int(fileUid)) {
@@ -66,21 +65,43 @@ func countPercent(opened int) float64 {
 	return float64(opened) * 100 / float64(rLimit.Max)
 }
 
+func interval() float64 {
+	b, err := ioutil.ReadFile("/etc/collectd/collectd.conf")
+	check(err)
+
+	s := string(b)
+	i, _ := regexp.Compile(`Interval`)
+
+	if i.MatchString(s) == true {
+		str := strings.Split(s, "\"")[1]
+		in, _ := strconv.Atoi(str)
+		if in == 0 {
+			return float64(60)
+		}
+		return float64(in)
+	}
+
+	return float64(60)
+}
+
+func hostname() string {
+	b, err := ioutil.ReadFile("/etc/collectd/collectd.conf")
+	check(err)
+
+	s := string(b)
+	h, _ := regexp.Compile(`Hostname`)
+
+	if h.MatchString(s) == true {
+		return strings.Split(s, "\"")[1]
+	}
+	return ""
+}
+
 func main() {
 	var username = flag.String("u", "user", "open files of username")
 	flag.Parse()
 	dirs := listProcs(username)
 	openFiles := countOpenFiles(dirs)
 	percent := countPercent(openFiles)
-	vl := api.ValueList{
-		Identifier: api.Identifier{
-			Host:   exec.Hostname(),
-			Plugin: "system",
-			Type:   "openfiles",
-		},
-		Time:     time.Now(),
-		Interval: exec.Interval(),
-		Values:   []api.Value{api.Gauge(percent)},
-	}
-	exec.Putval.Write(context.Background(), &vl)
+	fmt.Printf("PUTVAL %s/system/openfiles interval=%f N:%f\n", hostname(), interval(), percent)
 }
